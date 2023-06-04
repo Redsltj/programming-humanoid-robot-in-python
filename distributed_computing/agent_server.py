f3a2db1e1629056ab55fc37b19764125eb290a0d
Redsltj <131369180+Redsltj@users.noreply.github.com>
2023-06-04 20:21:11 +0200

    Update agent_server.py

--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -16,8 +16,13 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
 
-from inverse_kinematics import InverseKinematicsAgent
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+import threading
 
+#import pack and unpack from client
+from agent_client import PackUnpack
+
+from inverse_kinematics import InverseKinematicsAgent
 
 class ServerAgent(InverseKinematicsAgent):
     '''ServerAgent provides RPC service
@@ -27,31 +32,51 @@ class ServerAgent(InverseKinematicsAgent):
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        return self.perception.joint[joint_name]
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        if joint_name in self.target_joints.keys():
+            self.target_joints[joint_name] = angle
+            return True
+        else
+            return False
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.posture
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
-        # YOUR CODE HERE
+        # YOUR CODE HERE       
+        self.init_time = None
+        self.keyframes = keyframes
+        self.keyframes_done = False
+       
+        while not self.keyframes_done:
+            pass
+
+        print("Keyframes done")
+        return True
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        return self.packing.pack(self.transforms[name])
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        self.set_transforms(effector_name, self.packing.unpack(transform))
+        self.execute_keyframes(keyframes=self.keyframes)
+        return True
 
 if __name__ == '__main__':
     agent = ServerAgent()
